7bb170b180ee92749d3aedad65c6657e20362d4b
Seokeey <seokeey0411@gmail.com>
2020-10-14 16:56:12 +0900

    remove swp files

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, requests
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
@@ -57,7 +57,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = requests.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('이메일 또는 비밀번호를 다시 확인해주세요.', 'danger')
     return render_template('login.html', title='Login', form=form)
